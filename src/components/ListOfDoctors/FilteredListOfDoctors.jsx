56a034e320736064e1bb8100d5c345f655cac76c
Jocelynn Hartwig <jocelynnhartwig@gmail.com>
2022-01-08 19:35:15 -0500

    fix: remove useGetDataByDepartmnet hook

--- src/components/ListOfDoctors/FilteredListOfDoctors.jsx
+++ src/components/ListOfDoctors/FilteredListOfDoctors.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useAllData } from "../../contexts/allData";
 import getDataByDepartment from "../../services/getDataByDepartment";
 import buttons from "../../data/buttons";
-import useGetDataByDepartment from "../../hooks/useGetDataByDepartment";
 import FilterButton from "../FilterButton/FilterButton";
 import DoctorInformation from "../DoctorInformation/DoctorInformation";
 
