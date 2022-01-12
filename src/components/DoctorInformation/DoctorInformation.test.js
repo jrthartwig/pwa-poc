c1fe511d09a02b3dd4a856727976e7fa96f4e071
jrthartwig <jocelynnhartwig@gmail.com>
2022-01-12 17:58:26 -0500

    fix: formatting

--- src/components/DoctorInformation/DoctorInformation.test.js
+++ src/components/DoctorInformation/DoctorInformation.test.js
@@ -1,22 +1,23 @@
-import '@testing-library/jest-dom'; 
-import React from 'react'; 
-import {render, screen} from '@testing-library/react';
-import DoctorInformation from './DoctorInformation';
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import DoctorInformation from "./DoctorInformation";
 
-describe('the doctor information component', () => {
-    it('renders with doctor information', () => {
-        const doctor =  [{
-            name: "Dr. Mario",
-            department: "3D World",
-            startTime: new Date(Date.UTC(2021, 12, 24, 7, 0)),
-            endTime: new Date(Date.UTC(2021, 12, 24, 20, 0)),
-            notes: "here are some notes",
-            role: "Doctor",
-            callOrder: 1,
-            pager: 123456,
-          }];
-        render(<DoctorInformation filteredDoctors={doctor} />)
-
-        expect(screen.getByText("Doctor Name: Dr. Mario")).toBeInTheDocument()
-    })
-})
+describe("given a list of doctors", () => {
+  it("then it renders with doctor information", () => {
+    const doctor = [
+      {
+        name: "Dr. Mario",
+        department: "3D World",
+        startTime: new Date(Date.UTC(2021, 12, 24, 7, 0)),
+        endTime: new Date(Date.UTC(2021, 12, 24, 20, 0)),
+        notes: "here are some notes",
+        role: "Doctor",
+        callOrder: 1,
+        pager: 123456,
+      },
+    ];
+    render(<DoctorInformation filteredDoctors={doctor} />);
+    expect(screen.getByText("Doctor Name: Dr. Mario")).toBeInTheDocument();
+  });
+});
