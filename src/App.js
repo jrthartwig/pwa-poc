fe965d59eaec977f17a769d5aa4d413697d03ebf
Jocelynn Hartwig <jocelynnhartwig@gmail.com>
2022-01-22 22:27:29 -0500

    wip: api v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import FilteredListOfDoctors from './components/FilteredListOfDoctors/FilteredListOfDoctors';
 import { AllDataProvider } from './contexts/allData';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/getalldoctorinformation`)).json();
+      setData(text);
+    })();
+  });
   return (
-    <AllDataProvider>
-      <FilteredListOfDoctors />
-    </AllDataProvider>
+    <div>{data[0].name}</div>
+    // <AllDataProvider>
+    //   <FilteredListOfDoctors />
+    // </AllDataProvider>
   );
 }
 
