18100f923ec29d4d2521057630844d75dc01b57c
Jocelynn Hartwig <jocelynnhartwig@gmail.com>
2022-01-23 16:33:10 -0500

    try: error handling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import FilteredListOfDoctors from './components/FilteredListOfDoctors/FilteredListOfDoctors';
-import { AllDataProvider } from './contexts/allData';
+import React, { useState, useEffect } from "react";
+import FilteredListOfDoctors from "./components/FilteredListOfDoctors/FilteredListOfDoctors";
+import { AllDataProvider } from "./contexts/allData";
 
 function App() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
 
-  useEffect(() => {
+  const getDoctors = async () => {
     (async function () {
-      const { text } = await( await fetch(`/api/getalldoctorinformation`)).json();
+      const { text } = await (
+        await fetch(`/api/getalldoctorinformation`)
+      ).json();
       setData(text);
     })();
+  }
+  
+  useEffect(() => {
+    try {
+      await getDoctors(); 
+    }
+    catch (error) {
+      console.log(error)
+    }
   });
   return (
     <div>{data[0]}</div>
